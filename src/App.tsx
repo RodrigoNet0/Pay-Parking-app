161efa17f781a894ca7edd6070d7d9b26fd8480b
Rodrigo Neto <134714036+RodrigoNet0@users.noreply.github.com>
2024-02-17 15:16:28 +0000

    commit init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import { Header} from './components/Header.tsx';
+
+
+function App() {
+ 
+  return (
+    <>
+      <Header/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+    return (
+<div className="bg-neutral-950 max-w-100 h-14">
+    <h1 className="text-white p-4">Parking Pay</h1>
+</div>
+    );
+}
