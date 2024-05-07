edd7acddbc4989955491726f891a3b5216ee8432
Rodrigo Neto <134714036+RodrigoNet0@users.noreply.github.com>
2024-05-07 15:56:33 +0000

    feature: create component dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,9 @@ function App() {
   return (
 
     <>
-      <Header/>
-      <Form/>
-      {/* <ClientSection clientName={undefined} /> */}
+      {/* <Header/>
+      <Form/> */}
+      <ClientSection />
     </>
   )
 }
